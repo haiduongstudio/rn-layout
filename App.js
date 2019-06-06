431b7e405ce6db88b33371701cbded7771f106a0
Hai Nguyen Tien <haitin4bk@gmail.com>
2019-06-06 20:31:24 +0700

    Commit #1

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  TouchableWithoutFeedback,
+  Animated,
+  Dimensions,
+  SafeAreaView
+} from 'react-native';
+
+let SCREEN_WIDTH = Dimensions.get('screen').width;
+let SCREEN_HEIGHT = Dimensions.get('screen').height;
+
+var images = [
+  { id: 1, src: require('./assets/1.jpg') },
+  { id: 2, src: require('./assets/2.jpg') },
+  { id: 3, src: require('./assets/3.jpg') },
+  { id: 4, src: require('./assets/4.jpg') }
+];
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView style={{ flex: 1 }}>
+        {images.map(image => {
+          return (
+            <TouchableWithoutFeedback key={image.id}>
+              <Animated.View
+                style={{
+                  height: SCREEN_HEIGHT - 150,
+                  width: SCREEN_WIDTH,
+                  padding: 15
+                }}
+              >
+                <Image
+                  source={image.src}
+                  style={{
+                    flex: 1,
+                    height: null,
+                    width: null,
+                    resizeMode: 'cover',
+                    borderRadius: 20
+                  }}
+                />
+              </Animated.View>
+            </TouchableWithoutFeedback>
+          );
+        })}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -14,6 +59,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
