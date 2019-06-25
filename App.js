57555bf2a96a9b94304ca0b5d3062e1d7829e953
Hai Nguyen Tien <haitin4bk@gmail.com>
2019-06-25 18:06:09 +0700

    Completed

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Slider } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    value: 0
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Slider
+          minimumValue={-10}
+          maximumValue={10}
+          step={1}
+          value={this.state.value}
+          onValueChange={value => this.setState({ value })}
+        />
+        <Text>Value: {this.state.value}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginLeft: 10,
+    marginRight: 10,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    alignItems: 'stretch',
+    justifyContent: 'center'
+  }
 });
