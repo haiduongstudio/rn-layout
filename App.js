38f0861bf1d2b6c3a1be6bf124ab6bf6cf9ee98e
Hai Nguyen Tien <haitin4bk@gmail.com>
2019-06-25 16:33:46 +0700

    Completed

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  TouchableWithoutFeedback
+} from 'react-native';
 
 export default function App() {
+  handlePress = () => {
+    // alert('Hello');
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableNativeFeedback
+        onPress={handlePress}
+        background={TouchableNativeFeedback.SelectableBackground()}
+      >
+        <View style={styles.btn}>
+          <Text style={styles.btnText}>Touchable Highlight</Text>
+        </View>
+      </TouchableNativeFeedback>
     </View>
   );
 }
@@ -12,8 +30,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginLeft: 10,
+    marginRight: 10,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  btn: {
+    backgroundColor: '#E53224',
+    padding: 10,
+    paddingLeft: 50,
+    paddingRight: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5
+  },
+  btnText: {
+    color: '#fff'
+  }
 });
