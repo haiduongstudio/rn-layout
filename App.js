b5cf4af8b84d60dd7316ba77e693aff3e56aba29
Hai Nguyen Tien <haitin4bk@gmail.com>
2019-06-28 00:27:11 +0700

    Save #4

--- App.js
+++ App.js
@@ -5,14 +5,21 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 import History from './components/History';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+
+const TabNavigator = createBottomTabNavigator({
+  History,
+  AddEntry
+});
+
+const AppContainer = createAppContainer(TabNavigator);
 
 export default function App() {
   return (
     <Provider store={createStore(reducer)}>
       <View style={{ flex: 1 }}>
-        {/* <AddEntry /> */}
         <View style={{ height: 20 }} />
-        <History />
+        <AppContainer />
       </View>
     </Provider>
   );
